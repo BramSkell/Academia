7b0d7f51d8cb02d458fd003968986d366f913394
Bramdev89 <abraham.v.morales@outlook.com>
2017-08-15 16:36:48 -0500

    Add details json (ProdDeta.js) and modify products json (ProdCat.js).

--- ProductsCatalog/ProductsCatalog.PublicWebSite/js/getDetails.js
+++ ProductsCatalog/ProductsCatalog.PublicWebSite/js/getDetails.js
@@ -1,7 +1,6 @@
 $(document).ready(function () {
     (function getID() {
-        var id = location.search.substr(1);
-        var res = {};
+        var id = location.search.substr(1);        
         id.split("&").forEach(function (part) {
             var item = part.split("=");
             ShowDetails(item[1]);
@@ -10,19 +9,37 @@ $(document).ready(function () {
 
     function ShowDetails(item) {
         var id = parseInt(item);
-        var deta = "",desc = "";
-        $.getJSON("js/products.json", function (data) {
+        var deta = "", desc = "",thumb ="";
+       /* $.getJSON("js/details.json", function (data) {
             $.each(data, function (key, obj) {
                 if (obj.id == id) {
-                    var prodDeta = obj;
-                    deta = '<figure><img src="' + prodDeta.image + '"><figcaption>' + prodDeta.title + '</figcaption>' +
-                        '<img src="img/"><img src="img/"><img src="img/"></figure>' +
-                        '<p>Modelo ' + prodDeta.model + '</p><p>Precio ' + prodDeta.price + '</p>';
-                    desc = '<p>' + prodDeta.description + '</p>';
+                    $.each(obj.minImg, function (key,img) {
+                        thumb += '<img class="thumb" src="' + img + '">';
+                    });
+                    //for (var i = 0; i < obj.minImg.length; i++) {
+                    //    thumb += '<img class="thumb" src="' + obj.minImg[i] + '">';
+                    //}                  
+                    deta = '<figure><img src="' + obj.image + '"><figcaption>' + obj.title + '</figcaption>' +
+                        thumb+' </figure>'+
+                        '<p>Modelo ' + obj.model + '</p><p>Precio ' + obj.price + '</p>';
+                    desc = '<p>' + obj.description + '</p>';
                 }
             });
             $("#details").append(deta);
             $("#description").append(desc);
+        }); */
+        $.each(myDetaCata, function (key, obj) {
+            if (obj.id == id) {
+                $.each(obj.minImg, function (key, img) {
+                    thumb += '<img class="thumb" src="' + img + '">';
+                });
+                deta = '<figure><img class="detaIMG" src="' + obj.image + '"><figcaption>' + obj.title + '</figcaption>' +
+                        thumb + ' </figure>' +
+                    '<p>Modelo ' + obj.model + '</p><p>Precio ' + obj.price + '</p></br></br><aside><p>Productos similares</p><img class="similar" src="img/download%20(1).jpg"> <img class="similar" src="img/download%20(1).jpg"> <img class="similar" src="img/download%20(1).jpg"></aside>';
+                desc = '<p>' + obj.description + '</p>';
+            }
         });
+        $("#details").append(deta);
+        $("#description").append(desc);
     }
 });
--- ProductsCatalog/ProductsCatalog.PublicWebSite/js/getProducts.js
+++ ProductsCatalog/ProductsCatalog.PublicWebSite/js/getProducts.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
     (function () {
         console.log("Se ejecuto");
         var content = "";
-        $.getJSON("js/products.json", function (data) {
+       /*$.getJSON("js/products.json", function (data) {
             
             $.each(data, function (key, obj) {
                 //console.log(obj.image+" "+obj.title+" "+obj.description);
@@ -12,7 +12,12 @@ $(document).ready(function () {
                 //console.log(content);
             });
             $("#products").append(content);
-        });        
+        });   */
+        $.each(myCatalog, function (key, obj) {
+            content += '<figure class="productIMG"><a href="details.html?=' + obj.id + '"><img src=\"' + obj.image + '\"></a>';
+            content += '<figcaption>' + obj.title + '</figcaption><br/><p>' + obj.description + '</p></figure>';
+        });
+        $("#products").append(content);
     })();
 
 });
